7680015ee9a15d1caf5b355ac84f3ecf3d005601
leavemealonemf <strangemisterio78@gmail.com>
2023-11-14 01:17:43 +0300

    init repo

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Express } from "express";
+import { Server } from 'http';
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
+import { ExeptionFilter } from "./errors/exeption.filter";
+
+export class App {
+    app: Express;
+    server!: Server;
+    port: number;
+    logger: LoggerService;
+    userController: UserController;
+    exeptionFilter: ExeptionFilter;
+
+    constructor(
+        logger: LoggerService,
+        userController: UserController,
+        exeptionFilter: ExeptionFilter,
+        ) {
+        this.app = express();
+        this.port = Number(process.env.PORT) || 5000;
+        this.logger = logger;
+        this.userController = userController;
+        this.exeptionFilter = exeptionFilter;
+    }
+
+    useRoutes() {
+        this.app.use('/users', this.userController.router);
+    }
+
+    useExeptionFilters() {
+        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+    }
+
+    public async init() {
+        this.useRoutes();
+        this.useExeptionFilters();
+        this.server = this.app.listen(this.port);
+        this.logger.log(`Server started on PORT: ${this.port}`);
+    }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response, Request } from "express";
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error.class";
+
+export class UserController extends BaseController {
+    constructor(logger: LoggerService) {
+        super(logger);
+        this.bindRoutes([
+            { path: '/register', method: 'post', func: this.register },
+            { path: '/login', method: 'post', func: this.login }
+        ])
+    }
+
+    login(req: Request, res: Response, next: NextFunction) {
+        // this.ok(res, 'login');
+        next(new HTTPError(401, "Ошибка авторизации", 'login'));
+    }
+
+    register(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'register');
+    } 
+}
