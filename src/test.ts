6c0f00631e2910d2b7c4c33323e41ec5355a90e3
leavemealonemf <strangemisterio78@gmail.com>
2023-11-15 20:19:45 +0300

    added inversify

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+
+function Test(target: Function) {
+    Reflect.defineMetadata('a', 1, target);
+    const meta = Reflect.getMetadata('a', target);
+    console.log(meta);
+}
+
+function Prop(target: Object, name: string) {}
+
+@Test
+class A {
+    @Prop prop!: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Response, Request } from "express";
 import { BaseController } from "../common/base.controller";
-import { LoggerService } from "../logger/logger.service";
 import { HTTPError } from "../errors/http-error.class";
+import {injectable, inject} from "inversify";
+import { ILogger } from "../logger/logger.interface";
+import { TYPES } from "../types";
 
+@injectable()
 export class UserController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger);
+    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+        super(loggerService);
         this.bindRoutes([
             { path: '/register', method: 'post', func: this.register },
             { path: '/login', method: 'post', func: this.login }
